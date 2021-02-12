f9ad0f49b3eb05e617d8ec844460cd43755a3c0b
Samuel Nkoom Amoah <sa.am@programmer.net>
2021-02-12 23:54:25 +0100

    feat(NotFound): add notFound page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :style="styles.container">
+    <div>
+      <img
+        class="ui image large"
+        src="../assets/not_found.svg" />
+      <h2
+        color="grey"
+        is="sui-header"
+        :style="styles.text">
+        Awwww... Did you miss your way?
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      styles: {
+        container: {
+          display: 'flex',
+          minHeight: '100vh',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        text: {
+          textAlign: 'center',
+        },
+      },
+    };
+  },
+};
+</script>
