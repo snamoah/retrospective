2fee2b739d99ca3d0ca9b9734d5e69f464f7338a
Samuel Nkoom Amoah <sa.am@programmer.net>
2021-02-12 18:32:47 +0100

    feat(): add semantic-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import SemanticUI from 'semantic-ui-vue';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(SemanticUI);
 
 new Vue({
   render: (h) => h(App),
